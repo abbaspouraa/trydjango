aa8c0712e7a96a36a1cc0bbd9b910c68327dc129
Amir Abbaspour <83620288+abbaspouraa@users.noreply.github.com>
2022-02-16 08:46:18 -0700

    add some urls

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Products(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default="This is cool!")
+    title = models.CharField(max_length=60)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    summary = models.TextField()
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view, room_view
+
 urlpatterns = [
+    path('room/', room_view, name= 'room'),
+    path('', home_view, name="home"),
     path('admin/', admin.site.urls),
 ]
