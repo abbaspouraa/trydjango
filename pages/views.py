d7b7aee70449f13909393885c5a22260bbf491ff
Amir Abbaspour <83620288+abbaspouraa@users.noreply.github.com>
2022-02-17 12:48:00 -0700

    django template

--- pages/views.py
+++ pages/views.py
@@ -20,6 +20,7 @@ def room_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
     my_context = {
         "my_text": "This is about us",
-        "my_number": 123
+        "my_number": 123,
+        "my_list": range(10)
     }
     return render(request, "about.html", my_context)
